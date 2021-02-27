5b6670835138fea62d59809e7455633b6b4bf5af
divinedab <33793935+scarletdiv0922@users.noreply.github.com>
2021-02-26 21:49:39 -0800

    Designed MainActivity

--- app/src/main/java/com/example/technovation2021/MainActivity.java
+++ app/src/main/java/com/example/technovation2021/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.technovation2021;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declare objects
+    Button loginButton;
+    Button signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize objects with elements from xml files
+        loginButton = findViewById(R.id.login_button);
+        signUpButton = findViewById(R.id.sign_up_button);
+
+        // When the Login and Sign Up buttons are clicked, take the user to CalendarActivity
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
