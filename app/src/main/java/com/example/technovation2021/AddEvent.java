2c9d1b4ed9bba4d4849f6e2554aae0c021539359
Divya Koya <koyadivya@gmail.com>
2021-03-09 23:48:37 -0800

    o TimePicker is working o Align fields in 'Add Event' screen

--- app/src/main/java/com/example/technovation2021/AddEvent.java
+++ app/src/main/java/com/example/technovation2021/AddEvent.java
@@ -15,26 +15,6 @@ import java.util.Calendar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
-class TimePickerFragment extends DialogFragment
-        implements TimePickerDialog.OnTimeSetListener {
-
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the current time as the default values for the picker
-        final Calendar c = Calendar.getInstance();
-        int hour = c.get(Calendar.HOUR_OF_DAY);
-        int minute = c.get(Calendar.MINUTE);
-
-        // Create a new instance of TimePickerDialog and return it
-        return new TimePickerDialog(getActivity(), this, hour, minute,
-                DateFormat.is24HourFormat(getActivity()));
-    }
-
-    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        // Do something with the time chosen by the user
-        Log.d("Time picker", "time picked");
-    }
-}
 
 public class AddEvent extends AppCompatActivity {
 
